186e03c217e1129b253fd356d46cfadffcf8c355
liang <3300582399@qq.com>
2023-06-07 17:03:51 +0800

    java环境

---
+++ 开发/java/环境/java.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME=/etc/local/java1.8/
+export JRE_HOME=/etc/local/java1.8/jre/
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar:$JRE_HOME/lib:$CLASSPATH
+export PATH=$JAVA_HOME/bin:$PATH
